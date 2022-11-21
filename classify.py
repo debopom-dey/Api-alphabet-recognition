4bc26c066eb943f54853c53d6d1c1fadee4a522a
debopom-dey <71516417+debopom-dey@users.noreply.github.com>
2022-11-21 23:41:23 +0530

    Update classify.py

--- classify.py
+++ classify.py
@@ -28,7 +28,7 @@ def get_prediction(image):
     
     min_pixel = np.percentile(image_bw_resized, pixel_filter)
     
-    image_bw_resized_inverted_scaled = np.clip(image_bw_resized_inverted-min_pixel, 0, 255)
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
     max_pixel = np.max(image_bw_resized)
     image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
 
